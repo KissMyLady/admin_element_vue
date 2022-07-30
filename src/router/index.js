daa8a37aa26f14cf22570b3b60d97151bbd54257
KissMyLady <kissmyladys@gmail.com>
2022-07-30 23:26:16 +0800

    delete(): 删减页面, 启动成功

--- src/router/index.js
+++ src/router/index.js
@@ -67,19 +67,6 @@ export const asyncRoutes = [
     },
     children: [
       {
-        path: 'create',
-        component: () => import('@/views/example/create'),
-        name: 'CreateArticle',
-        meta: { title: 'Create Article', icon: 'edit' }
-      },
-      {
-        path: 'edit/:id(\\d+)',
-        component: () => import('@/views/example/edit'),
-        name: 'EditArticle',
-        meta: { title: 'Edit Article', noCache: true, activeMenu: '/example/list' },
-        hidden: true
-      },
-      {
         path: 'list',
         component: () => import('@/views/example/list'),
         name: 'ArticleList',
